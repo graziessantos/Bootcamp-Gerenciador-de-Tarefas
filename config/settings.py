1161b3a6886108627fdf73ab344dc4ebc362fa0e
graziessantos <grazie.santoss467@gmail.com>
2026-05-17 17:20:52 -0300

    feat: adiciona configurações para deploy no Render

--- config/settings.py
+++ config/settings.py
@@ -77,3 +77,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
